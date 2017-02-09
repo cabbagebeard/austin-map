c865bd6fcf97eac7375e47269b35d9342d50300a
Kevin Windisch <kwindisch1992@gmail.com>
2017-02-09 15:54:50 -0600

    Init commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+function initMap() {
+  var austin = {lat: 30.266568, lng: -97.743202};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    minZoom: 4,
+    center: austin
+  });
+  infoWindow = new google.maps.InfoWindow();
+}
+
+function markers() {
+  var vegMarker = new google.maps.Marker({
+    icon: 'img/darkgreen_MarkerV.png',
+    map: map
+  });
+  var barMarker = new google.maps.Marker({
+    icon: 'img/yellow_MarkerB.png',
+    map: map
+  });
+  var regMarker = new google.maps.Marker({
+    map: map
+  });
+}
+
+
+try {
+  initMap();
+} catch (err) {
+  // places.errorMessages.push("Failure loading Map");
+}
